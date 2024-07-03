b11cfd75846b0c7b0d7c6bdc4031f65c453b796d
Jeremy Muhlich <jmuhlich@bitflood.org>
2024-07-03 14:40:34 -0400

    Harden bash script

--- preview.sh
+++ preview.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-python tenable_convert.py "$@" \
+set -euo pipefail
+
+base=$(dirname "$0")
+
+python "$base"/tenable_convert.py "$@" \
     | xsv select 'DNS Name,IP Address,Path' \
     | xsv sort \
     | xsv table
