464b19486cd9030167562c2761e5736fbf9e5dbf
Jeremy Muhlich <jmuhlich@bitflood.org>
2024-08-02 13:07:50 -0400

    Add support for plugin output with package name

--- preview.sh
+++ preview.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 base=$(dirname "$0")
 
 python "$base"/tenable_convert.py "$@" \
-    | xsv select 'DNS Name,IP Address,Path' \
+    | xsv select 'DNS Name,IP Address,Location,FixedVersion,Plugins' \
     | xsv sort \
     | xsv table
--- tenable_convert.py
+++ tenable_convert.py
@@ -10,11 +10,14 @@ df = pd.read_excel(sys.argv[1])
 g = pd.merge(
     df.fillna(''),
     df["Plugin Output"]
-    .str.extractall(r"Path\s*: (?P<Path>.*)$", re.MULTILINE)
+    .str.extractall(
+        r"(?:Path|Installed package)\s*: (?P<Location>.*?)\n.*?Fixed (?:version|package)\s*: (?P<FixedVersion>.*?)$",
+        re.MULTILINE | re.DOTALL,
+    )
     .reset_index(names=['Index', 'Match']),
     left_index=True,
     right_on='Index',
-).groupby(["IP Address", "MAC Address", "DNS Name", "NetBIOS Name", "Path"])[
+).groupby(["IP Address", "MAC Address", "DNS Name", "NetBIOS Name", "Location", "FixedVersion"])[
     ["Plugin", "Plugin Name", "Severity"]
 ]
 out = (
